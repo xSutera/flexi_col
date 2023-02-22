34346cb5da090e487a7fe98bca5585f22b137163
xSutera <710ctl@gmail.com>
2023-02-22 11:07:33 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+var x = 5;
+    
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there");
+}
+    
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
